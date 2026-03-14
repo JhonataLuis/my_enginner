1512ab63e21b93e6e38afe527c249d5aec109078
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-14 15:10:14 -0300

    implementado views.py
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- anuncios/views.py
+++ anuncios/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Anuncio, ImagemAnuncio
+from .forms import AnuncioForm
 
 # Create your views here.
+def home(request):
+    # Mostra todos os anuncios, do mais novo para o mais velho
+    anuncios = Anuncio.objects.all().order_by('-data_criacao')
+
+    return render(request, 'anuncios/home.html', {'anuncios': anuncios})
