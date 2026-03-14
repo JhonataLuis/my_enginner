35bd682d208ad380879fa36bf99114a9a88b7234
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-14 19:27:32 -0300

    pagina de detalhes_anuncio criada
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- anuncios/urls.py
+++ anuncios/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('anuncio/<int:pk>/', views.detalhe_anuncio, name='detalhe_anuncio'),
     path('criar/', views.criar_anuncio, name='criar_anuncio'),
+    path('detalhes/', views.detalhe_anuncio, name='detalhe_anuncio')
 ]
