c39677e7714e017566626d25106da8a16bbcbcfd
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-14 17:25:00 -0300

    ajustado paginas web
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- imobiliaria/urls.py
+++ imobiliaria/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('anuncios.urls')),
+    path('anuncios', include('anuncios.urls')),
 ]
 
 if settings.DEBUG:
