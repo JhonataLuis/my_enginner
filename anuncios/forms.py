2d8bbf36dc5d69a325ca460642820a703a052220
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-14 15:09:32 -0300

    implementado forms
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- anuncios/forms.py
+++ anuncios/forms.py
@@ -1,13 +1,15 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from .models import Anuncio, ImagemAnuncio
 
-class CadastroForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True, label='Nome')
-    last_name = forms.CharField(max_length=30, required=True, label='Sobrenome')
+class AnuncioForm(forms.ModelForm):
 
     class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1','password2']
+        model = Anuncio
+        fields = [
+            'titulo', 'descricao', 'preco', 'tipo_imovel',
+            'endereco', 'cidade', 'quartos', 'banheiros', 'area_total'
+        ]
+
+        widgets = {
+            
+        }    
